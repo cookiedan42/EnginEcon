ba63271c1fdc16701578a1625e5a6956d4c06af1
daniel <cookiedan42@gmail.com>
2020-03-03 10:47:10 +0800

    first upload

--- ie2111.py
+++ ie2111.py
@@ -21,13 +21,6 @@ from numpy import irr,mirr,nper,rate
 #Doing a start/end of period variable following numpy convention
 
 #A series
-def __sanitize_rate(rate):
-    if type(rate) == str and "%" in rate: 
-        return float(rate.split("%")[0].strip())/100
-    elif type(rate) in (int,float):
-        return rate
-    else:
-        raise SyntaxError("invalid rate")
 
 def _sanitize_rate(rate):
     if type(rate) == str and "%" in rate: 
@@ -39,48 +32,48 @@ def _sanitize_rate(rate):
 
 
 def FP(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return (1+i)**n
 
 def PF(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return (1+i)**-n
 
 def FA(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return ((1+i)**n-1)/i
 
 def PA(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return ((1+i)**n-1)/(i*(1+i)**n)
 
 def AF(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return FA(i,n)**-1
 
 def AP(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return PA(i,n)**-1
 
 #G series
 def PG(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return (1/i)*(((1+i)**n-1)/(i*(1+i)**n) - (n/(1+i)**n))
 
 def AG(rate,n,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return 1/i - n/((1+i)**n-1)
 
 def PGEO(rate,n,f,when=0):
-    i = __sanitize_rate(rate)
+    i = _sanitize_rate(rate)
     n = (n-1 if when == 1 else n)
     return 1/(i-f) * (1-PF(i,n)*FP(f,n))
 ###########################################################################################################################
