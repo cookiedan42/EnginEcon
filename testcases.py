bbb0fd0d4266c48cef5e14d896c1f7e96325fed0
daniel <cookiedan42@gmail.com>
2020-03-16 16:31:52 +0800

    ATCF test case

--- testcases.py
+++ testcases.py
@@ -35,3 +35,14 @@ assert F.get_AW(0.1) == F.get_PW(0.1) * ie.AP(0.1,5)
 assert F.get_FW(0.1) == F.get_PW(0.1) * ie.FP(0.1,5)
 assert (A+B).IRR() == irr((A+B).get_mergedSeries())
 assert G.IRR(A) == irr([g-a for g,a in zip(G.get_mergedSeries(),A.get_mergedSeries())])
+
+#ATCF test case based on assignment
+annual = ie.createA(-10000,15)
+salvage = ie.createSingle(10,5000) + ie.createSingle(15,30000)
+BTCF = annual+salvage
+cap = ie.tax.createDepreciation(-100000,0,3) + ie.tax.createDepreciation(-100000,10,3)
+ATCF = ie.tax.AfterTaxCashFlow(BTCF,cap,lambda x:0.17*x)
+assert [round(i,2) for i in ATCF.get_mergedSeries()] == [-100000.0, -2633.33, -2633.33, -2633.33, -8300.0, -8300.0, -8300.0, -8300.0, -8300.0, -8300.0, -104150.0, -2633.33, -2633.33, -2633.33, -8300.0, 16600.0]
+assert round(ATCF.get_PW("5%"),2) == -208111.93
+
+print('ALL CLEAR')
