773ac2d056a8612860df40e5c24783e18940f088
daniel <cookiedan42@gmail.com>
2020-03-16 13:28:52 +0800

    update testcase for new syntax

--- testcases.py
+++ testcases.py
@@ -13,13 +13,13 @@ assert round(ie.PG  (0.05,5),3) == 8.237
 assert round(ie.AG  (0.05,5),4) == 1.9025
 #assert round(ie.PGEO(0.05,5),4) == #ok IDK how to test case this
 
-A = ie.createA(rate=0.1,pmt=5,length=5) 
-B = ie.createB(rate=0.1,pmt=-5,length=5)
-G = ie.createG(rate=0.1,g=5,length=5)
-F = ie.createGeo(rate=0.1,start=1,f=1,length=5)
-R = ie.createRepeat(rate=0.1,pattern=[0,1],length=5)
-S = ie.createSingle(rate=0.1,pos=5,value=5)
-I = ie.createSingle(rate=0.1,pos=0,value=-5)
+A = ie.createA(pmt=5,length=5) 
+B = ie.createB(pmt=-5,length=5)
+G = ie.createG(g=5,length=5)
+F = ie.createGeo(start=1,f=1,length=5)
+R = ie.createRepeat(pattern=[0,1],length=5)
+S = ie.createSingle(pos=5,value=5)
+I = ie.createSingle(pos=0,value=-5)
 
 assert A.get_mergedSeries() == [0,5,5,5,5,5]
 assert B.get_mergedSeries() == [-5,-5,-5,-5,-5,0]
@@ -30,8 +30,8 @@ assert S.get_mergedSeries() == [0,0,0,0,0,5]
 assert I.get_mergedSeries() == [-5]
 assert (A+B).get_mergedSeries() == [-5,0,0,0,0,5]
 
-assert F.get_PW() == sum(F.get_mergedSeries()[i]*ie.PF(0.1,i) for i in range(len(F.get_mergedSeries())))
-assert F.get_AW() == F.get_PW() * ie.AP(0.1,5)
-assert F.get_FW() == F.get_PW() * ie.FP(0.1,5)
+assert F.get_PW(0.1) == sum(F.get_mergedSeries()[i]*ie.PF(0.1,i) for i in range(len(F.get_mergedSeries())))
+assert F.get_AW(0.1) == F.get_PW(0.1) * ie.AP(0.1,5)
+assert F.get_FW(0.1) == F.get_PW(0.1) * ie.FP(0.1,5)
 assert (A+B).IRR() == irr((A+B).get_mergedSeries())
 assert G.IRR(A) == irr([g-a for g,a in zip(G.get_mergedSeries(),A.get_mergedSeries())])
