c5f82b679181cb0083ad5a98e11c20e0a9fd32c2
daniel <cookiedan42@gmail.com>
2020-03-17 21:16:35 +0800

    montecarlo has distribution support too

--- monteCarlo.py
+++ monteCarlo.py
@@ -1,48 +1,93 @@
 import scipy.stats as st
 import numpy as np
 
-def discretePMF(values,probs,count):
-    return st.rv_discrete(values=(values,probs)).rvs(size=count)
+# distributions return a array of size n if count=n
+# else return an object
+# object.rvs(n) to get an array of size n
+def discretePMF(values,probs,count=None):
+    if count:
+        return st.rv_discrete(values=(values,probs)).rvs(size=count)
+    else:
+        return st.rv_discrete(values=(values,probs))
 
-def uniform(lower,upper,count):
-    return st.uniform.rvs(loc=lower,scale = upper-lower,size=count)
+def uniform(lower,upper,count=None):
+    if count:
+        return st.uniform(loc=lower,scale = upper-lower).rvs(size=count)
+    else:
+        return st.uniform(loc=lower,scale = upper-lower)
 
-def uniform_int(lower,upper,count):
-    return st.randint.rvs(low=lower,high=upper,size=count)
+def uniform_int(lower,upper,count=None):
+    if count:
+        return st.randint(low=lower,high=upper).rvs(size=count)
+    else:
+        return st.randint(low=lower,high=upper)
 
-def triangle(lower,mode,upper,count):
-    return st.triang.rvs(loc=lower,c=(mode-lower)/(upper-lower),scale=upper-lower,size=count)
+def triangle(lower,mode,upper,count=None):
+    if count:
+        return st.triang(loc=lower,c=(mode-lower)/(upper-lower),scale=upper-lower).rvs(size=count)
+    else:
+        return st.triang(loc=lower,c=(mode-lower)/(upper-lower),scale=upper-lower)
 
-def normal(mean,stdvar,count):
-    return st.norm.rvs(loc=mean,scale=stdvar,size=count)
+def normal(mean,stdvar,count=None):
+    if count:
+        return st.norm(loc=mean,scale=stdvar).rvs(size=count)
+    else:
+        return st.norm(loc=mean,scale=stdvar)
 
-def trunc_normal(mean,stdvar,lower,upper,count):
+def trunc_normal(mean,stdvar,lower,upper,count=None):
     a,b = (lower - mean)/stdvar , (upper - mean)/stdvar
-    return st.truncnorm.rvs(a,b,loc=mean,scale=stdvar,size=count)
+    if count:
+        return st.truncnorm(a,b,loc=mean,scale=stdvar).rvs(size=count)
+    else:
+        return st.truncnorm(a,b,loc=mean,scale=stdvar)
 
-def fixed(value,count):
-    return uniform(value,value,count)
+def fixed(value,count=None):
+    if count:
+        return st.uniform(loc=value,scale = 0).rvs(size=count)
+    else:
+        return st.uniform(loc=value,scale = 0)
 
-def lognormal(mean,stdvar,count):
+def lognormal(mean,stdvar,count=None):
     a_mean = 0.5* np.log(mean**4/(stdvar**2+mean**2))
     a_stdvar = np.sqrt(np.log(stdvar**2/mean**2+1))
-    return st.lognorm.rvs(s=a_stdvar,scale=np.exp(a_mean),size=count)
+    if count:
+        return st.lognorm(s=a_stdvar,scale=np.exp(a_mean)).rvs(size=count)
+    else:
+        return st.lognorm(s=a_stdvar,scale=np.exp(a_mean))
 
 class risk(object):
     #so basically this is a template for slotting in functions and data
-    def __init__(self,func,data):
+    def __init__(self,func,data,counts=None):
         self.func = func
         self.data = data
-        self.seq  = self.updateSeq()
-    def updateSeq(self):
+        self.seq  = self.updateSeq(counts)
+    def updateSeq(self,counts=None):
         self.errorCount = 0
+        self.errorLog = []
         variables = self.func.__code__.co_varnames[:self.func.__code__.co_argcount]
-        sortedvars =  tuple(tuple(self.data[k][i]for k in variables) for i in range(len(self.data[list(self.data.keys())[0]])))
+        if not counts:
+            countsList = []
+            for i in self.data.values():
+                try:
+                    countsList += [len(i)]
+                except:
+                    countsList += [0]
+            counts = max(countsList)        
+        sortedvars = ()
+        for i in range(counts):
+            ith = ()
+            for key in variables:
+                if type(self.data[key]) not in (list,tuple,np.ndarray):
+                    ith += (self.data[key],)
+                else:
+                    ith += (self.data[key][i],)
+            sortedvars+=(ith,)
         out = ()
         for i in sortedvars:
             try:
                 out+=(self.func(*i),)
-            except:
+            except Exception as e:
+                self.errorLog +=[e]
                 self.errorCount+=1
         return np.array(out)
     def updateData(self,data):
